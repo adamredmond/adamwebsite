72fc75d597f9f82d64bc365475ef1c5d6a16d576
adamredmond <adam-redmond@outlook.com>
2021-07-26 22:42:39 +0100

    commit lel

---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../App.css';
+import Cards from '../Cards';
+import HeroSection from '../HeroSection';
+import Footer from '../Footer';
+
+function Home() {
+  return (
+    <>
+    <a>hello</a>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
